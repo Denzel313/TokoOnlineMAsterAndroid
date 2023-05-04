1d30321ac6e0cc20e02121b7ad2adf0ddc38fa9d
Denzel <maple4064@gmail.com>
2023-05-05 01:21:39 +0700

    vid 3

--- app/src/main/java/com/example/tokoonline/MainActivity.kt
+++ app/src/main/java/com/example/tokoonline/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.tokoonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.tokoonline.fragment.AkunFragment
 import com.example.tokoonline.fragment.HomeFragment
 import com.example.tokoonline.fragment.KeranjangFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +20,10 @@ class MainActivity : AppCompatActivity() {
     private val fm: FragmentManager = supportFragmentManager
     private var active: Fragment = fragmentHome
 
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +31,28 @@ class MainActivity : AppCompatActivity() {
         fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
         fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
         fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+        when(item.itemId) {
+            R.id.navigation_home->{
+                Log.d("Respons","Home")
+            }
+            R.id.navigation_keranjang->{
+                Log.d("Respons","Keranjang")
+            }
+            R.id.navigation_akun->{
+                Log.d("Respons","Akun")
+            }
+        }
+
+            false
+        }
 
     }
 }
