9216537ce9997640bc325542e2f0778a8a538f2a
Denzel313 <maple4064@gmail.com>
2023-05-04 23:14:08 +0700

    vid 3

--- app/src/main/java/com/example/tokoonline/MainActivity.kt
+++ app/src/main/java/com/example/tokoonline/MainActivity.kt
@@ -10,16 +10,19 @@ import com.example.tokoonline.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
 
-    val fragmentHome: Fragment = HomeFragment()
-    val fragmentAkun: Fragment = AkunFragment()
-    val fragmentKeranjang: Fragment = KeranjangFragment()
-    val fm: FragmentManager = supportFragmentManager
-    var active: Fragment = fragmentHome
+    private val fragmentHome: Fragment = HomeFragment()
+    private val fragmentAkun: Fragment = AkunFragment()
+    private val fragmentKeranjang: Fragment = KeranjangFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragmentHome
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+
     }
 }
